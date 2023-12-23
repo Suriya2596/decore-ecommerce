9062487068d81b3c1b2cad0ecee10991cae95554
Suriya2596 <suriyajagan25@gmail.com>
2023-12-23 23:20:19 +0530

    installed tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,21 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "@material-tailwind/react";
+import { Link } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
+      <div className='flex justify-around'>
+        <Link href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
+        </Link>
+        <Link href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </Link>
       </div>
       <h1>Vite + React</h1>
+      <Button className='bg-[#328746] text-[#fff] hover:bg-[#4e71bc]'>Button</Button>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
